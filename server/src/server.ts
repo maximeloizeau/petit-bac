6c206f89528ab580183b6fe684cd3a3db80db031
Maxime Loizeau <maxime@usehero.com>
2020-04-13 19:24:57 +0100

    chore: update server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,8 @@ import websocketApp from "./websocketApp";
 const server = new Server(app);
 websocketApp(server);
 
-server.listen(process.env.PORT || 8888, () => {
+const port = parseInt(process.env.PORT || "") || 8888;
+server.listen(port, () => {
   console.log("App is running");
 });
 
